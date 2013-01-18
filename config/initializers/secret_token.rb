fbcb6b2088b8ec76abdaf7b2b3829e2e8c1a8eb8
Benedikt Müller <mueller@me.com>
2013-01-18 16:07:07 +0100

    Created Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SporesApp::Application.config.secret_token = '05291e6b6504c1db7febf2b810bda48366fe53730291bd01cefa03362b5f663934ea52c39e33bf5c54cbfcaca537deb35bb2d32021cc520a9eda13e92ffb94bb'
